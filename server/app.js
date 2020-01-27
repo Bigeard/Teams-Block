b2de7f14c6348554b50e5af7669f2c1d20a44814
JeremDlcrn <44464165+JeremDlcrn@users.noreply.github.com>
2020-01-27 18:12:38 +0100

    Starting Back-end

--- server/app.js
+++ server/app.js
@@ -10,3 +10,15 @@ document.querySelector('#nextButton').addEventListener('click', ()=>{
 	i = i+1;
 	document.querySelector('#actualPage').value = i;
 });
+
+
+
+
+db.get(`SELECT data FROM bloc WHERE id='${range}';`, (err, result) => {
+	if (err) {
+		console.log(err);
+	}
+	else {
+		//result
+	}
+})
